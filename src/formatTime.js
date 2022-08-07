3d60e2e85ceccbd971ef26ceb38393f8bb3cf393
Rafał Porowski <rporowski@interia.pl>
2022-08-07 17:03:16 +0200

    ADD files to project

---
+++ src/formatTime.js
@@ -0,0 +1,5 @@
+function formatTime(seconds) {
+    return [parseInt(seconds / 60 / 60), parseInt((seconds / 60) % 60), parseInt(seconds % 60)].join(":").replace(/\b(\d)\b/g, "0$1")
+}
+
+export default formatTime
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 // DEMO JOKE APP
-import generateJoke from './generateJoke'
-import './styles/main.scss'
-import laughing from './assets/laughing.svg'
+import formatTime from "./formatTime"
+import "./styles/main.scss"
 
-const laughImg = document.getElementById('laughImg')
-laughImg.src = laughing
+const durationTime = document.getElementsByClassName("durationTime")
+const durationTimearr = [...durationTime]
+const el = document.querySelector('[data-id="sort"]')
 
-const jokeBtn = document.getElementById('jokeBtn')
-jokeBtn.addEventListener('click', generateJoke)
+durationTimearr.forEach(element => {
+    element.innerHTML = formatTime(element.innerHTML)
+})
 
-generateJoke()
+formatTime()
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1
+    const tBody = table.tBodies[0]
+    const rows = Array.from(tBody.querySelectorAll("tr"))
+
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+        const aColumnText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim()
+        const bColumnText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim()
+
+        return aColumnText > bColumnText ? 1 * dirModifier : -1 * dirModifier
+    })
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild)
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows)
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("asc", "desc"))
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("asc", asc)
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("desc", !asc)
+}
+
+el.addEventListener("click", () => {
+    let headerCol
+    const tableElement = el.parentElement.parentElement.parentElement.parentElement
+    const headerIndex = document.getElementsByTagName("th")
+    const headerId = [...headerIndex]
+
+    headerId.forEach(element => {
+        if (element.contains(el)) {
+            headerCol = headerId.indexOf(element)
+        }
+    })
+
+    const currentIsAscending = el.parentElement.classList.contains("asc")
+    sortTableByColumn(tableElement, headerCol, !currentIsAscending)
+})
